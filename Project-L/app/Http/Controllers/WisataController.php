e89dc74f806ab50281a62b17ba64c758a63a987d
Rimuru <filbertasherdarius@gmail.com>
2025-05-24 20:40:19 +0700

    wisata model

---
+++ Project-L/app/Http/Controllers/WisataController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\wisata;
+use Illuminate\Http\Request;
+
+class WisataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(wisata $wisata)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(wisata $wisata)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, wisata $wisata)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(wisata $wisata)
+    {
+        //
+    }
+}
